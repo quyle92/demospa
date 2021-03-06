79a07a991713ab3a9c3dc7ee26f414a77d2e537f
Quy Le <free2idol@gmail.com>
2021-03-06 13:36:52 +0700

    updated

--- helper/security.php
+++ helper/security.php
@@ -7,10 +7,17 @@ if( !isset($_SESSION['MaNV'])  )
    	alert("Bạn ko được quyền truy cập vào đây!");
    	 setTimeout(
         function(){
-            window.location = "./login.php" 
+            window.location = "../../../login.php" 
         },
     100);
    	</script>');
+}
+
+//if the page is DoiMatKhau, then no need further investigation as it is not a kind of report.
+if( explode('/', $p)[1] === 'DoiMatKhau' ){
+  
+  return;
+
 }
 
 if( $_SESSION['MaNV'] !== 'HDQT' && !in_array($page_name, $bao_cao_duoc_xem) )
@@ -19,8 +26,25 @@ if( $_SESSION['MaNV'] !== 'HDQT' && !in_array($page_name, $bao_cao_duoc_xem) )
    	alert("Bạn ko được quyền truy cập vào đây!");
    	 setTimeout(
         function(){
-            window.location = "./login.php" 
+            window.location = "../../../login.php" 
         },
     100);
    	</script>');
 }
+
+// if (!isset($_SESSION['CREATED'])) {
+//     $_SESSION['CREATED'] = time();
+// } elseif (isset($_SESSION['CREATED']) && (time() - $_SESSION['CREATED'] > 2)) {
+//     // last request was more than 30 minutes ago
+//     unset($_SESSION['MaNV']);
+//     unset($_SESSION['CREATED']);
+//     setcookie(session_name(), '', time() - 2592000, '/'); 
+//     die('<script> 
+//     alert("Phiên đăng nhập đã hết! Vui lòng login lại!");
+//      setTimeout(
+//         function(){
+//             window.location = "../../../login.php" 
+//         },
+//     100);
+//     </script>');
+// }
